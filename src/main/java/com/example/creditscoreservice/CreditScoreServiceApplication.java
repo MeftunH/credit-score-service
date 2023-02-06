85cdb2a8bf8c261bad25de544e2f17462c66ef00
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-06 09:36:10 +0300

    Init project

---
+++ src/main/java/com/example/creditscoreservice/CreditScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditscoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoreServiceApplication.class, args);
+	}
+
+}
