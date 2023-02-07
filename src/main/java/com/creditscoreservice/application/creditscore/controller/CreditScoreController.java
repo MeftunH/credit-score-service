9cbcda58e7d8c36aed61d016c7ba7e4e9c8b36d8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-07 06:04:00 +0300

    FEAT: add get Credit Score

--- src/main/java/com/creditscoreservice/application/creditscore/controller/CreditScoreController.java
+++ src/main/java/com/creditscoreservice/application/creditscore/controller/CreditScoreController.java
@@ -4,24 +4,22 @@ package com.creditscoreservice.application.creditscore.controller;
 import com.creditscoreservice.application.creditscore.dto.CreditScoreDTO;
 import com.creditscoreservice.application.creditscore.dto.CreditScoreSaveRequestDTO;
 import com.creditscoreservice.application.creditscore.service.CreditScoreService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/credit-score")
-@RequiredArgsConstructor
 public class CreditScoreController {
     private final CreditScoreService creditScoreService;
 
-    @PostMapping("/save")
-    public ResponseEntity<CreditScoreDTO> saveCreditScore(@RequestBody @Valid CreditScoreSaveRequestDTO creditScoreSaveRequestDTO) {
-        CreditScoreDTO creditScoreDTO = creditScoreService.save(creditScoreSaveRequestDTO);
+    public CreditScoreController(CreditScoreService creditScoreService) {
+        this.creditScoreService=creditScoreService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CreditScoreDTO> getCreditScore(@RequestBody CreditScoreSaveRequestDTO creditScoreSaveRequestDTO) {
+        CreditScoreDTO creditScoreDTO = creditScoreService.getCreditScore(creditScoreSaveRequestDTO);
         return new ResponseEntity<>(creditScoreDTO, HttpStatus.CREATED);
     }
 }
