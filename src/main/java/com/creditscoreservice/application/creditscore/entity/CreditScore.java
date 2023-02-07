183fd5dc0620aac39b8fc2c145905e5d84110cca
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-08 01:20:37 +0300

    FIX: generated value on entity

--- src/main/java/com/creditscoreservice/application/creditscore/entity/CreditScore.java
+++ src/main/java/com/creditscoreservice/application/creditscore/entity/CreditScore.java
@@ -13,6 +13,7 @@ import lombok.Setter;
 public class CreditScore {
     @Id
     @SequenceGenerator(name = "creditScoreSeq", sequenceName = "credit_score_seq")
+    @GeneratedValue(generator="creditScoreSeq", strategy = GenerationType.SEQUENCE)
     private Long id;
 
    @Column(name = "creditScore", nullable = false)
