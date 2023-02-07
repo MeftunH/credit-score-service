e25e1decc4ded9df5b4ee77329677a67ea791f7b
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-07 06:02:14 +0300

    PERF: Fix Serializable

--- src/main/java/com/creditscoreservice/application/creditscore/dto/CreditScoreSaveRequestDTO.java
+++ src/main/java/com/creditscoreservice/application/creditscore/dto/CreditScoreSaveRequestDTO.java
@@ -1,17 +1,15 @@
 package com.creditscoreservice.application.creditscore.dto;
-/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.Serializable;
 import java.util.Date;
+
 @Data
-public class CreditScoreSaveRequestDTO {
+public class CreditScoreSaveRequestDTO implements Serializable {
     @NotNull
     private String customerName;
     @NotNull
