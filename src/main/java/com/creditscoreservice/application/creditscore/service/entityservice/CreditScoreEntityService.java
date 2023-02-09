b64a75555694dd5d4042c0537bd43ec155f80e66
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 08:37:59 +0300

    FIX: set as interface

--- src/main/java/com/creditscoreservice/application/creditscore/service/entityservice/CreditScoreEntityService.java
+++ src/main/java/com/creditscoreservice/application/creditscore/service/entityservice/CreditScoreEntityService.java
@@ -2,25 +2,8 @@ package com.creditscoreservice.application.creditscore.service.entityservice;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
 import com.creditscoreservice.application.creditscore.entity.CreditScore;
-import com.creditscoreservice.application.creditscore.repository.CreditScoreRepository;
-import com.creditscoreservice.application.creditscore.util.CalculateCreditScore;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Service
-@RequiredArgsConstructor
-public class CreditScoreEntityService {
-
-    private final CreditScoreRepository creditScoreRepository;
-    private final CalculateCreditScore calculateCreditScore;
-
-    public void save(CreditScore creditScore) {
-         creditScoreRepository.save(creditScore);
-    }
-
-
-    public int getCreditScore() {
-        return calculateCreditScore.nextInt();
-    }
+public interface CreditScoreEntityService {
+    void save(CreditScore creditScore);
+     int getCreditScore();
 }
