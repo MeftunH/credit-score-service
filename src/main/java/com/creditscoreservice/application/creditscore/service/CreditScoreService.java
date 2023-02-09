3f446afc9c6a2e7aa7d274f3dd572b8c9dc24feb
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 08:37:18 +0300

    FEAT: set as interface

--- src/main/java/com/creditscoreservice/application/creditscore/service/CreditScoreService.java
+++ src/main/java/com/creditscoreservice/application/creditscore/service/CreditScoreService.java
@@ -3,10 +3,6 @@ package com.creditscoreservice.application.creditscore.service;
 
 import com.creditscoreservice.application.creditscore.dto.CreditScoreDTO;
 import com.creditscoreservice.application.creditscore.dto.CreditScoreSaveRequestDTO;
-import com.creditscoreservice.application.creditscore.entity.CreditScore;
-import com.creditscoreservice.application.creditscore.mapper.CreditScoreMapper;
-import com.creditscoreservice.application.creditscore.service.entityservice.CreditScoreEntityService;
-import org.springframework.stereotype.Service;
 
 public interface CreditScoreService {
     CreditScoreDTO saveCreditScore(CreditScoreSaveRequestDTO creditScoreSaveRequestDTO);
